7492afca57a3bccedded5eb810964e6d479643d2
OlegSmac <68604410+OlegSmac@users.noreply.github.com>
2023-10-09 16:13:53 +0300

    backend

--- backend/schemaUser.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const schemaUser = new mongoose.Schema({
-    name: {type: String, required: true}
-});
-
-export default mongoose.model('schemaUser', schemaUser, 'users');
-
